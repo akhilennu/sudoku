6c9ca84f97e92179daa7eb626a40f9a94b9dcaf6
DESKTOP-4IHMTBP\akhil <akhil.e2012@gmail.com>
2020-06-13 11:56:11 +0530

    Updates

--- assets/index.f686ca25.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var o=t(0),c=t.n(o),a=t(2);t(8);var r=()=>c.a.createElement("div",{className:"App"},c.a.createElement("h1",null,"Welcome to sudoku"));Object(a.render)(c.a.createElement(r,null),document.getElementById("root"))}},[[3,1,2]]]);
